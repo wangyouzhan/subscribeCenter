ab885086f751e5492ae6fb98b0d53e8dd1990da0
wangyouzhan <wangyouzhan1987@163.com>
2016-12-09 21:14:48 +0800

    json parse...

--- subscribeCenter/JsonViewController.m
+++ subscribeCenter/JsonViewController.m
@@ -54,7 +54,16 @@
     
      NSString *jsonArray2 = @"{\"user\":[{\"name\":\"wang\",\"age\":\"23\"},{\"name\":\"wangd\",\"age\":\"23\"}]}";
     
-    NSData *jsonData = [jsonArray2 dataUsingEncoding:NSUTF8StringEncoding];
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"json" ofType:nil];
+    NSFileManager *fileMgr = [NSFileManager defaultManager];
+    if ([fileMgr fileExistsAtPath:filePath]) {
+        jsonStr = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+    }else{
+        self.textView.text = @"文件没找到";
+    }
+    
+    
+    NSData *jsonData = [jsonStr dataUsingEncoding:NSUTF8StringEncoding];
     id jsonObj = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingAllowFragments error:nil];
     
     if ([jsonObj isKindOfClass:[NSDictionary class]]) {
@@ -62,17 +71,16 @@
         NSDictionary *dic = (NSDictionary*)jsonObj;
 //         self.textView.text = [dic objectForKey:@"name"];
         
-        NSArray *temp = [dic objectForKey:@"user"];
+//        NSArray *temp = [dic objectForKey:@"weatherinfo"];
+        NSDictionary *temp = [dic objectForKey:@"weatherinfo"];
         
-        for (NSDictionary *p in temp) {
-            self.textView.text = [p objectForKey:@"name"];
-        }
+        self.textView.text = [temp objectForKey:@"city"];
         
+//        for (NSDictionary *p in temp) {
+//            self.textView.text = [p objectForKey:@"city"];
+//        }
         
-        
-        
-        
-        
+    
 //        dispatch_async(dispatch_get_main_queue(), ^{
 //            self.textView.text = [[dic objectForKey:@"name"] stringByAppendingString:@"wnag300000"];
 //        });
@@ -84,9 +92,8 @@
         for(NSDictionary *dic in arr){
             self.textView.text = [dic objectForKey:@"name"];
         }
-        
-        
     }
+    
     
     
     
